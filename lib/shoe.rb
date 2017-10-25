a0e580eb5fc70c751d276fdf856ff54fd7ea5f45
Monica  Karasek <mlkarasek@gmail.com>
2017-10-25 04:39:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
 
-  attr_accessor :brand, :color, :size, :material, :condition
-
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :condition
 
   def initialize(brand)
     @brand = brand
