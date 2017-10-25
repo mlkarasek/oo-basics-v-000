2a247bd9f4d45efa4357ccbabb409eeae4e1f985
Monica  Karasek <mlkarasek@gmail.com>
2017-10-25 04:28:48 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,9 +4,23 @@ class Book
     @title = title
   end
 
-
   def title
     @title 
   end
 
+  def author=(author)
+    @author = author 
+  end 
+
+  def author 
+    @author 
+  end
+
+  def page_count(page_count)
+    @page_count = page_count
+  end 
+
+  def page_count
+    @page_count
+  end 
 end
